d0edb66579e46be69554a168535b50f14db23658
Alexandre Zajac <work@alexandrezajac.com>
2020-08-31 00:25:00 +0200

    Deliver first usable version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import Select from 'react-select';
+import moment from 'moment';
+import { momentLocalizer, Calendar } from 'react-big-calendar';
+import getCourseData from './Hooks';
+
+const localizer = momentLocalizer(moment);
 
 function App() {
+  const [state, setState] = useState(null);
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  useEffect(() => {
+    const asyncCourseData = async () => {
+      const courseData = await getCourseData();
+      setState(courseData);
+    };
+    asyncCourseData();
+  }, []);
+  const handleChange = (options) => {
+    const newOptions = options === null ? [] : options;
+    setSelectedCourses(newOptions);
+  };
+  const mapToSelectable = (dataPoint) => {
+    const { ectsPerQuarter, courseTitle } = dataPoint;
+    return {
+      value: dataPoint,
+      label: `${courseTitle} (${ectsPerQuarter} ECTS)`,
+    };
+  };
+  const getQuarterECTS = (quarterCourses) => {
+    const byQuarter = quarterCourses
+      .map(mapToSelectable)
+      .filter((c) => selectedCourses.map((sc) => sc.label).includes(c.label))
+      .map(({ value: { ectsPerQuarter } }) => ectsPerQuarter);
+    return byQuarter.reduce((a, b) => a + b, 0);
+  };
+  const p1Ects = state ? getQuarterECTS(state.p1Courses) : 0;
+  const p2Ects = state ? getQuarterECTS(state.p2Courses) : 0;
+  const p3Ects = state ? getQuarterECTS(state.p3Courses) : 0;
+  const totalEcts = p1Ects + p2Ects + p3Ects;
+  const calendarEvents = state
+    ? state.courseCalendar
+        .filter((course) =>
+          selectedCourses
+            .map((sc) => sc.value.courseTitle)
+            .includes(course.courseTitle)
+        )
+        .map(({ htmlLink, courseTitle, startTime, endTime }) => ({
+          title: courseTitle,
+          start: new Date(startTime),
+          end: new Date(endTime),
+          htmlLink,
+        }))
+    : [];
+  console.log(state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="col">
+        {state && (
+          <>
+            <div className="quarter-container">
+              <p className="primary">Courses for P1</p>
+              <Select
+                className="select-container"
+                onChange={handleChange}
+                value={state.p1Courses
+                  .map(mapToSelectable)
+                  .filter((c) =>
+                    selectedCourses.map((sc) => sc.label).includes(c.label)
+                  )}
+                isMulti
+                options={state.p1Courses.map(mapToSelectable)}
+              />
+              <p>
+                Total ECTS:
+                {` ${p1Ects}`}
+              </p>
+            </div>
+            <div className="quarter-container">
+              <p className="primary">Courses for P2</p>
+              <Select
+                className="select-container"
+                onChange={handleChange}
+                value={state.p2Courses
+                  .map(mapToSelectable)
+                  .filter((c) =>
+                    selectedCourses.map((sc) => sc.label).includes(c.label)
+                  )}
+                isMulti
+                options={state.p2Courses.map(mapToSelectable)}
+              />
+              <p>
+                Total ECTS:
+                {` ${p2Ects}`}
+              </p>
+            </div>
+            <div className="quarter-container">
+              <p className="primary">Courses for P3</p>
+              <Select
+                className="select-container"
+                value={state.p3Courses
+                  .map(mapToSelectable)
+                  .filter((c) =>
+                    selectedCourses.map((sc) => sc.label).includes(c.label)
+                  )}
+                onChange={handleChange}
+                isMulti
+                options={state.p3Courses.map(mapToSelectable)}
+              />
+              <p>
+                Total ECTS:
+                {` ${p3Ects}`}
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="col" style={{ flex: 3 }}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Total ECTS:
+          {` ${totalEcts}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {state && (
+          <Calendar
+            localizer={localizer}
+            defaultView="week"
+            onSelectEvent={({ htmlLink }) => window.open(htmlLink, '_blank')}
+            min={new Date(2017, 9, 0, 9, 0, 0)}
+            max={new Date(2017, 9, 0, 21, 0, 0)}
+            events={calendarEvents}
+            style={{ marginLeft: '2rem', width: '90%' }}
+          />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Hooks.js
@@ -0,0 +1,136 @@
+import axios from 'axios';
+
+const months = [
+  'september',
+  'october',
+  'november',
+  'december',
+  'january',
+  'february',
+  'march',
+  'april',
+];
+const timeMonths = [
+  '2020-09',
+  '2020-10',
+  '2020-11',
+  '2020-12',
+  '2021-01',
+  '2021-02',
+  '2021-03',
+  '2021-04',
+  '2021-05',
+];
+const halfECTSCourses = [
+  'Hidden Markov models and Sequential Monte Carlo methods N.Chopin ENSAE',
+  'Enchères et matching : apprentissage et approximations V.Perchet ENSAE',
+  'Statistical Learning Theory J.Mourtada ENSAE',
+  'High dimensional statistics A.Tsybakov ENSAE',
+];
+const notCourses = [
+  'Examens P1',
+  'EXAMENS',
+  'Examens',
+  'Congés',
+  'Départ en stage',
+  'Cours intensifs',
+  'P1',
+  'P2',
+  'P3',
+  'Remise à niveau ',
+  'Journée de rentrée',
+  'FERIE',
+  'CONGES ',
+  'Examen Deep learning I G.Peeters',
+];
+
+const p1End = new Date('2020-11-15T23:00:00');
+const p2End = new Date('2021-01-31T23:00:00');
+
+const getQuarter = (startTime) => {
+  const compareDate = new Date(startTime);
+  if (compareDate < p1End) return 'P1';
+  if (compareDate < p2End) return 'P2';
+  return 'P3';
+};
+
+const getEctsPerQuarter = (courseTitle) => {
+  if (halfECTSCourses.includes(courseTitle)) return 1.25;
+  if (courseTitle === 'Data Camp') return 5;
+  return 2.5;
+};
+
+const mapToSelectable = (dataPoint) => {
+  const { ectsPerQuarter, courseTitle } = JSON.parse(dataPoint);
+  return {
+    value: courseTitle,
+    label: `${courseTitle} (${ectsPerQuarter} ECTS)`,
+  };
+};
+
+const getCourseData = async () => {
+  const courseCalendar = await Promise.all(
+    months.map(async (m, i) => {
+      const response = await axios.get(
+        `https://clients6.google.com/calendar/v3/calendars/1rs7sh06s9nerdi7v2rg754g1g@group.calendar.google.com/events?calendarId=1rs7sh06s9nerdi7v2rg754g1g%40group.calendar.google.com&singleEvents=true&timeZone=Europe%2FParis&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${
+          timeMonths[i]
+        }-03T00%3A00%3A00%2B02%3A00&timeMax=${
+          timeMonths[i + 1]
+        }-07T00%3A00%3A00%2B02%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`
+      );
+      const {
+        data: { items },
+      } = response;
+      const monthItems = [];
+      items.forEach((item) => {
+        const {
+          htmlLink,
+          summary,
+          start: { dateTime: startTime },
+          end: { dateTime: endTime },
+        } = item;
+        const courseTitle = summary.replace(/\t/g, '');
+        const ectsPerQuarter = getEctsPerQuarter(courseTitle);
+        monthItems.push({
+          htmlLink,
+          courseTitle,
+          startTime,
+          endTime,
+          ectsPerQuarter,
+        });
+      });
+      return monthItems;
+    })
+  );
+  const concatenatedCalendar = [].concat(...courseCalendar);
+  const coursePerQuarter = {
+    P1: new Set(),
+    P2: new Set(),
+    P3: new Set(),
+  };
+  concatenatedCalendar.forEach(({ courseTitle, ectsPerQuarter, startTime }) => {
+    if (!notCourses.includes(courseTitle)) {
+      const quarter = getQuarter(startTime);
+      coursePerQuarter[quarter].add(
+        JSON.stringify({ ectsPerQuarter, courseTitle, quarter })
+      );
+    }
+  });
+  const p1Courses = [...coursePerQuarter.P1].map((dataPoint) =>
+    JSON.parse(dataPoint)
+  );
+  const p2Courses = [...coursePerQuarter.P2].map((dataPoint) =>
+    JSON.parse(dataPoint)
+  );
+  const p3Courses = [...coursePerQuarter.P3].map((dataPoint) =>
+    JSON.parse(dataPoint)
+  );
+  return {
+    p1Courses,
+    p2Courses,
+    p3Courses,
+    courseCalendar: concatenatedCalendar,
+  };
+};
+
+export default getCourseData;
